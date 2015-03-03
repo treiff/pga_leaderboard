9d2c1eaabb5d31bfceb110a4a3549b2e4bd19215
Trevor Reiff <trevorreiff@gmail.com>
2015-03-03 10:49:32 -0500

    Update specs, modify .travis

--- spec/pga_leaderboard_spec.rb
+++ spec/pga_leaderboard_spec.rb
@@ -8,30 +8,36 @@ module PgaLeaderboard
       expect(tournament).to be_a(Tournament)
     end
 
-    it 'should return an array when parsed' do
-      expect(tournament.parsed_array).to be_a(Array)
+    describe '#parsed_array' do
+      it 'should return an array when parsed' do
+        expect(tournament.parsed_array).to be_a(Array)
+      end
     end
 
-    it 'returns tournament name as string' do
-      expect(tournament.tournament_name).to be_a(String)
+    describe '#clean_response' do
+      it 'should return a scrubbed array' do
+        expect(tournament.clean_response).to be_a(Array)
+      end
+
+      it 'removes any prepended text before = sign' do
+        expect(tournament.clean_response[4]).not_to include("=")
+      end
+
+      it 'decodes each item to remove URI encoding' do
+        expect(tournament.clean_response[4]).not_to include("%20")
+      end
     end
 
-    it 'returns tournament name scrubbed of identifier' do
-      expect(tournament.tournament_name).not_to include("golf")
+    describe '#heading' do
+      it 'extracts the first two array elements' do
+        expect(tournament.heading.length).to eq(2)
+      end
     end
 
-    it 'returns the last updated time as string' do
-      expect(tournament.last_update).to be_a(String)
+    describe '#body' do
+      it 'returns remaining array elements' do
+        expect(tournament.body.length).to eq(tournament.clean_response.length - tournament.heading.length)
+      end
     end
-  end
-
-  describe CLI do
-    it 'is initialized with a tournament'
-  end
-
-  describe Scheduler do
-    it 'takes time in seconds argument'
-    it 'runs a task after x amount of seconds'
-    it 'reruns the task until exited'
   end
 end
